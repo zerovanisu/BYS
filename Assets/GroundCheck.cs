a5e451258907c213f5e719fb11f6bb7176359dc8
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2020-07-03 01:01:00 +0900

    BYS

---
+++ Assets/GroundCheck.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private string groundTag = "Ground";
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsGround()
+    {
+        if(isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if(isGroundExit)
+        {
+            isGround = false;
+        }
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+        return isGround;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundEnter = true;
+            //Debug.Log("何かが判定に入りました");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundStay = true;
+            //Debug.Log("何かが判定に入り続けています");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+            //Debug.Log("何かが判定をでました");
+        }
+    }
+}
